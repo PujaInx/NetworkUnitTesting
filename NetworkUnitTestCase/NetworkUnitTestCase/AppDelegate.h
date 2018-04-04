cb407277b2f89413ea670270a63bfb998c214ddb
Intelivex Labs <puja@intelivexlabs.com>
2018-04-04 12:31:20 +0530

    initial project creation

---
+++ NetworkUnitTestCase/NetworkUnitTestCase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkUnitTestCase
+//
+//  Created by Intelivex Labs on 04/04/18.
+//  Copyright © 2018 Intelivex Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetworkUnitTestCase/NetworkUnitTestCase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkUnitTestCase
+//
+//  Created by Intelivex Labs on 04/04/18.
+//  Copyright © 2018 Intelivex Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
